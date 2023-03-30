808406e0abc9c2591ac0c47eab95436dcf65f657
ShubhamGour4616 <92658279+ShubhamGour4616@users.noreply.github.com>
2023-03-30 10:07:18 +0530

    Update url.js

--- src/components/Utils/url.js
+++ src/components/Utils/url.js
@@ -1,3 +1,3 @@
 export const STRAPI_API_TOKEN = process.env.REACT_APP_STRAPI_API_TOKEN;
 
-export const API_URL =process.env.REACT_APP_API_URL;
+export const API_URL="https://free-store.onrender.com";
