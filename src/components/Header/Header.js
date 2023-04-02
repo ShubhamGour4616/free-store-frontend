000b3e17120c69aac006bfedcf101be13925bbb5
ShubhamGour4616 <92658279+ShubhamGour4616@users.noreply.github.com>
2023-04-02 18:51:39 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
 
   return (
 
-<div className="sticky top-0 bg-white z-50">
+<div className="sticky top-0 bg-red-200 z-50">
     <header className="w-full h-16 flex justify-between py-3 drop-shadow-xl z-50 ">
     <Link to="/"> <section className="text-3xl px-2 font-bold  hover:text-cyan-400 ease-in-out duration-300"> FREESTORE</section></Link> 
         <section  className="flex text-3xl mx-5 ">
